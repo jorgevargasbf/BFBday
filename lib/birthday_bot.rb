e3f0a7ec4ca6bfb163578f948a7e43ed904083c3
jorge <jorge.vargas@blankfactor.com>
2021-05-10 18:07:42 -0500

    bf changes

--- lib/birthday_bot.rb
+++ lib/birthday_bot.rb
@@ -1,4 +1,4 @@
-require 'httparty'
+#require 'httparty'
 require 'config_reader'
 require 'birthday_reader'
 
@@ -17,11 +17,12 @@ class BirthdayBot
     puts "Checking who was born today (#{today.to_s})"
     birthdays.each do |b|
       if (b[3].to_i == today.month) && (b[4].to_i == today.day)
-        message = "#{@config.greeting_message} #{b[0]} #{b[1]}" 
-        HTTParty.post(@config.slack_url, body: { channel: @config.channel_name,
-                                                 username: @config.bot_name,
-                                                 text: message,
-                                                 icon_emoji: @config.bot_emoji }.to_json)
+        message = "#{@config.greeting_message} #{b[0]} #{b[1]}"
+        puts message
+        #HTTParty.post(@config.slack_url, body: { channel: @config.channel_name,
+         #                                        username: @config.bot_name,
+          #                                       text: message,
+           #                                      icon_emoji: @config.bot_emoji }.to_json)
       end
     end
   end
